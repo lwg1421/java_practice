0648ec0dee4eb358422c42778da5274f846b8001
wonkkkn <lwg1421@naver.com>
2023-01-07 14:29:24 +0900

    첫 파일

---
+++ java_tutorials/src/org/opentutorials/javatutorials/array/ArrayLoopDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.array;
+
+public class ArrayLoopDemo {
+
+	public static void main(String[] args) {
+		String[] members = {"최진혁", "최유빈", "이원근"};
+		for (int i = 0;i<members.length;i++) {
+			System.out.println(members[i]+"가 상담을 받았습니다.");
+		}
+
+	}
+
+}
