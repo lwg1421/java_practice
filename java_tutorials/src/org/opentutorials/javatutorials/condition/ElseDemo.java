6f313d58b33cc24bb269c4ebb660f6af63b4b6f7
wonkkkn <lwg1421@naver.com>
2023-01-07 14:43:26 +0900

    조건문 학습내용

---
+++ java_tutorials/src/org/opentutorials/javatutorials/condition/ElseDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.condition;
+
+public class ElseDemo {
+
+	public static void main(String[] args) {
+		String a = "Hello world";
+		String b = "Hello world";
+		if (a.equals(b)) {
+			System.out.println(1);
+		} else if(true) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+
+	}
+
+} 
