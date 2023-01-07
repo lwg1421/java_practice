d939c2ec4e6819bb36a0cc27ab1d034966d35d32
wonkkkn <lwg1421@naver.com>
2023-01-07 14:42:35 +0900

    loop 학습내용

---
+++ java_tutorials/src/org/opentutorials/javatutorials/loop/WhileDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.loop;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<=10){
+			System.out.println(i+"번째 출력합니다.");
+			i++;
+		}
+
+	}
+
+}
