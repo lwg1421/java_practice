b7391d56579bf6149c9dda8df79e6ddbf69fe3d8
wonkkkn <lwg1421@naver.com>
2023-01-09 20:03:47 +0900

    파일 내 상속, 같은 패키지 내 다른 파일 클래스 상속 학습내용

--- java_tutorials/src/org/opentutorials/javatutorials/Inheritance/example1/CalculatorDemo2.java
+++ java_tutorials/src/org/opentutorials/javatutorials/Inheritance/example1/CalculatorDemo2.java
@@ -11,7 +11,7 @@ public class CalculatorDemo2 {
 
 	public static void main(String[] args) {
 		MultiplicationableCalculator c1 = new MultiplicationableCalculator();
-        c1.setOprands(10, 20);
+
         c1.sum();
         c1.avg();
         c1.multiplication();
---
+++ java_tutorials/src/org/opentutorials/javatutorialsInheritance/example4/ConstructorDemo.java
@@ -0,0 +1,49 @@
+package org.opentutorials.javatutorialsInheritance.example4;
+
+//부모 클래스
+class Calculator{
+	int left, right;
+	
+//public Calculator(int left, int right) {
+//	this.left = left;
+//	this.right = right; 
+//}
+
+public void setOprands(int left, int right) {
+	this.left = left;
+	this.right = right;
+}
+	
+ public void sum() {
+     System.out.println(this.left + this.right);
+ }
+
+ public void avg() {
+     System.out.println((this.left + this.right) / 2);
+ }
+ 
+}
+
+//자식 클래스
+//부모 클래스에 있는 내용을 상속받음
+class SubstractionableCalculator extends Calculator{
+public void substract() {
+     System.out.println(this.left - this.right);
+ 	}
+}
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		
+		SubstractionableCalculator c1 = new SubstractionableCalculator();
+		// 부모클래스의 내용을 끌어와 사용 가능
+		c1.setOprands(80,5);
+		c1.avg();
+		c1.sum();
+		c1.substract();
+	}
+		
+
+	
+}
