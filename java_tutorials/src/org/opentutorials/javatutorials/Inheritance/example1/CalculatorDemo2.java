dac2d16672636427befcac014faccf703812593b
wonkkkn <lwg1421@naver.com>
2023-01-09 19:28:34 +0900

    같은 패키지 내 다른 파일에서 정의된 클래스 상속 학습내용

---
+++ java_tutorials/src/org/opentutorials/javatutorials/Inheritance/example1/CalculatorDemo2.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.Inheritance.example1;
+
+// 해당 파일 내에는 없지만 같은 패키지 안에 정의되어 있으므로 Calculator의 인용이 가능하다.
+class MultiplicationableCalculator extends Calculator {
+    public void multiplication() {
+        System.out.println(this.left * this.right);
+    }
+}
+
+public class CalculatorDemo2 {
+
+	public static void main(String[] args) {
+		MultiplicationableCalculator c1 = new MultiplicationableCalculator();
+        c1.setOprands(10, 20);
+        c1.sum();
+        c1.avg();
+        c1.multiplication();
+
+	}
+
+}
