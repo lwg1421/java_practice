4b7743c9b8abb48d8e67491a810b92611dc572d4
wonkkkn <lwg1421@naver.com>
2023-02-09 21:58:41 +0900

    변수

---
+++ java_tutorials/src/com/java/pretraining/no2/variables1.java
@@ -0,0 +1,16 @@
+package com.java.pretraining.no2;
+
+public class variables1 {
+
+	public static void main(String[] args) {
+		
+		// int : 자료형 
+		// number : 변수명 
+		// = : 대입 
+		// 5 : 대입값 
+		// ; : 문장종료 
+		int number = 5;
+
+	}
+
+}
