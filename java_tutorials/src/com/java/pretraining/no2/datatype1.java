714871a810829ab920e59ae4b1780fef0edec338
wonkkkn <lwg1421@naver.com>
2023-02-10 18:43:21 +0900

    자료형 학습내용

---
+++ java_tutorials/src/com/java/pretraining/no2/datatype1.java
@@ -0,0 +1,26 @@
+package com.java.pretraining.no2;
+
+public class datatype1 {
+
+	public static void main(String[] args) {
+		
+		// 1. 논리형
+		boolean name1 = true;
+		
+		// 2. 문자형
+		// 문자가 하나인 경우에는 char로 지정
+		char name2 = 'a';
+		// 한 문자가 아닌 문자열인 경우 String으로 지정
+		String name6 = "abc";
+		
+		// 3. 정수형
+		int name3 = 8;
+		
+		// 4. 실수형
+		double name4 = 3.14;
+		// float의 경우 소수 뒤에 f를 붙여 형변환을 해주어야 한다.
+		float name5 = 1.5f;
+
+	}
+
+}
