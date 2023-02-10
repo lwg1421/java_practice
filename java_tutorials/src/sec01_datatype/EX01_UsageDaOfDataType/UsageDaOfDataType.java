86b9a3140e69d08cb4b7a8845cb78fea5cb451ad
wonkkkn <lwg1421@naver.com>
2023-02-11 01:58:28 +0900

    변수 선언과 값을 대입하는 두 가지 방법

---
+++ java_tutorials/src/sec01_datatype/EX01_UsageDaOfDataType/UsageDaOfDataType.java
@@ -0,0 +1,16 @@
+package sec01_datatype.EX01_UsageDaOfDataType;
+
+public class UsageDaOfDataType {
+
+	public static void main(String[] args) {
+		// 변수 선언과 동시에 값 대입
+		int a = 3;
+		/// 변수 선언과 값 대입 따로
+		int b;
+		b = 4;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
