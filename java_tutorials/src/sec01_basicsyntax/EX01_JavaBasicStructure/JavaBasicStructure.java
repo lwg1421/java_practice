d31ab0919f2e5ded7daf9915be0fc422836a28a3
wonkkkn <lwg1421@naver.com>
2023-02-11 01:06:53 +0900

    생성 프로젝트의 기본 구조

---
+++ java_tutorials/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,17 @@
+package sec01_basicsyntax.EX01_JavaBasicStructure;
+
+/* 
+ * 여러줄 주석을 하는 방법
+ * Ctrl + Shift + '/'
+*/
+
+public class JavaBasicStructure {
+
+	public static void main(String[] args) {
+		// 1줄 주석을 하는 방법
+		// Ctrl + '/'
+		System.out.println("콘솔 화면에 출력");
+
+	}
+
+}
