95102b58d90fef12545be509d7fc65e805067836
wonkkkn <lwg1421@naver.com>
2023-02-11 01:49:02 +0900

    컴파일 후 생성되는 바이트코드(.class)

---
+++ java_tutorials/src/sec01_basicsyntax/EX02_ByteCodeFiles/ByteCodeFiles.java
@@ -0,0 +1,22 @@
+package sec01_basicsyntax.EX02_ByteCodeFiles;
+
+class A{
+	// A.class
+}
+class B{
+	// B.class
+}
+class C{
+	// C.class
+	class D{
+		//C$D.class
+	}
+}
+public class ByteCodeFiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
