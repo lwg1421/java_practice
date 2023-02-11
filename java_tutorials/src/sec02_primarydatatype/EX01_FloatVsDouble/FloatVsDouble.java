8dd7bb2d78198434dbad75dbdb28e0991fdd1646
wonkkkn <lwg1421@naver.com>
2023-02-11 18:39:00 +0900

    float과 double의 정밀도 비교

---
+++ java_tutorials/src/sec02_primarydatatype/EX01_FloatVsDouble/FloatVsDouble.java
@@ -0,0 +1,20 @@
+package sec02_primarydatatype.EX01_FloatVsDouble;
+
+public class FloatVsDouble {
+
+	public static void main(String[] args) {
+		// float의 정밀도(대략 소수 7자리)
+		float f1 = 1.0000001f;
+		System.out.println(f1); // 정상출력
+		float f2 = 1.00000001f;
+		System.out.println(f2); // 오차발생
+		
+		// double의 정밀도(대략 소수 15자리)
+		double d1 = 1.000000000000001;
+		System.out.println(d1); // 정상출력
+		double d2 = 1.0000000000000001;
+		System.out.println(d2); // 오차발생
+
+	}
+
+}
