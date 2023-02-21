5de25c723209310442816faf9625efc58e7ba2a8
wonkkkn <lwg1421@naver.com>
2023-02-22 00:10:59 +0900

    연습

---
+++ java_tutorials/src/sec05_referencedatatype/array/EX04_PrimaryAndReferenceType/PrimaryAndReferenceType.java
@@ -0,0 +1,12 @@
+package sec05_referencedatatype.array.EX04_PrimaryAndReferenceType;
+
+public class PrimaryAndReferenceType {
+
+	public static void main(String[] args) {
+		double a = 26;
+		double b = 35;
+		System.out.println(a/b);
+
+	}
+
+}
