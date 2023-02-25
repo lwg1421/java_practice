514bd4797395fbd538292619cd56e6bd0c5430ea
wonkkkn <lwg1421@naver.com>
2023-02-25 18:10:43 +0900

    String 객체를 생성하는 두가지 방법

--- java_tutorials/src/sec05_referencedatatype/string/EX01_CreateStringObject/CreateStringObject.java
+++ java_tutorials/src/sec05_referencedatatype/string/EX01_CreateStringObject/CreateStringObject.java
@@ -3,7 +3,13 @@ package sec05_referencedatatype.string.EX01_CreateStringObject;
 public class CreateStringObject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//#1. String 객체 생성 1
+		String str1 = new String("안녕");
+		System.out.println(str1);
+		
+		//#2. String 객체 생성 2
+		String str2 = "안녕하세요";
+		System.out.println(str2);
 
 	}
 
