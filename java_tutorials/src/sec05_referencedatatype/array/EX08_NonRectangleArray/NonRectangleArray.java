ae31652aed8ddcf8a77243cab33b7491b4d4f16f
wonkkkn <lwg1421@naver.com>
2023-02-25 18:04:17 +0900

    2차원 비정방 행렬 배열의 3가지 원소 값 대입 방법

---
+++ java_tutorials/src/sec05_referencedatatype/array/EX08_NonRectangleArray/NonRectangleArray.java
@@ -0,0 +1,10 @@
+package sec05_referencedatatype.array.EX08_NonRectangleArray;
+
+public class NonRectangleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
