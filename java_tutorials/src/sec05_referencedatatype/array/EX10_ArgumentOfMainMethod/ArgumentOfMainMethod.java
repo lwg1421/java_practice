c489068c14612b028dea76086e383aa525162b7e
wonkkkn <lwg1421@naver.com>
2023-02-25 18:09:00 +0900

    main 메소드로의 입력 매개변수 전달

---
+++ java_tutorials/src/sec05_referencedatatype/array/EX10_ArgumentOfMainMethod/ArgumentOfMainMethod.java
@@ -0,0 +1,10 @@
+package sec05_referencedatatype.array.EX10_ArgumentOfMainMethod;
+
+public class ArgumentOfMainMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
