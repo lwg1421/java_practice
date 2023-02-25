d324c3d59137000c2e3d18ab9456d70b633dca4c
wonkkkn <lwg1421@naver.com>
2023-02-25 18:10:29 +0900

    String 객체를 생성하는 두가지 방법

---
+++ java_tutorials/src/sec05_referencedatatype/string/EX01_CreateStringObject/CreateStringObject.java
@@ -0,0 +1,10 @@
+package sec05_referencedatatype.string.EX01_CreateStringObject;
+
+public class CreateStringObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
