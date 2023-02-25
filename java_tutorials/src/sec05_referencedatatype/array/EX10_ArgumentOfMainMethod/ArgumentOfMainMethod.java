8c0e91ea3faf4e2894de88ec72a207e6e8db40bd
wonkkkn <lwg1421@naver.com>
2023-02-25 18:09:29 +0900

    main 메서드로의 입력매개변수 전달

--- java_tutorials/src/sec05_referencedatatype/array/EX10_ArgumentOfMainMethod/ArgumentOfMainMethod.java
+++ java_tutorials/src/sec05_referencedatatype/array/EX10_ArgumentOfMainMethod/ArgumentOfMainMethod.java
@@ -3,7 +3,22 @@ package sec05_referencedatatype.array.EX10_ArgumentOfMainMethod;
 public class ArgumentOfMainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//#1. args 배열의 길이 구하기
+		System.out.println(args.length);
+		System.out.println();
+		
+		//#2. 매개변수 출력 1
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]);
+		}
+		//System.out.println(Integer.parseInt(args[1])+1); //4??? 31
+		System.out.println();		
+		
+		//#3. 매개변수 출력 2
+		for(String s : args) {
+			System.out.println(s);
+		}
+		System.out.println();
 
 	}
 
