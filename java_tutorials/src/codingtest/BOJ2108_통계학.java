f0ea57d9aac7a806bc850ea2b92be4dd861d2bb3
wonkkkn <lwg1421@naver.com>
2023-03-29 21:07:16 +0900

    백준 2108

--- java_tutorials/src/codingtest/BOJ2108_통계학.java
+++ java_tutorials/src/codingtest/BOJ2108_통계학.java
@@ -1,10 +1,11 @@
 package codingtest;
 
+import java.util.Scanner;
+
 public class BOJ2108_통계학 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
