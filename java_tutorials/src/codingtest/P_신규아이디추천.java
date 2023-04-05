7f15868cf58df0d5fd9350746f0fa07697dd6570
wonkkkn <lwg1421@naver.com>
2023-04-05 23:31:13 +0900

    프로그래머스 신규아이디추천

--- java_tutorials/src/codingtest/P_신규아이디추천.java
+++ java_tutorials/src/codingtest/P_신규아이디추천.java
@@ -1,34 +1,37 @@
 package codingtest;
 
-import java.util.Scanner;
-
 //단계
-//1. 대문자 -> 소문자로
-//2. 소문자,숫자, -, _, .만 남기고 다 제거
-//3. ..은 .으로 치환
-//4. .이 처음이나 끝에 있으면 제거
-//5. id가 빈 문자열이면 a 대입
-//6. 16자 이상이면 앞에서 15글자까지만 자름
-//7. .이 끝에 위치하면 . 제거
-//8. id가 2자 이하라면 마지막 문자를 길이가 3이 될 때 까지 반복
+// 1. 대문자 -> 소문자로
+// 2. 소문자,숫자, -, _, .만 남기고 다 제거
+// 3. ..은 .으로 치환
+// 4. .이 처음이나 끝에 있으면 제거
+// 5. id가 빈 문자열이면 a 대입
+// 6. 16자 이상이면 앞에서 15글자까지만 자름
+// 7. .이 끝에 위치하면 . 제거
+// 8. id가 2자 이하라면 마지막 문자를 길이가 3이 될 때 까지 반복
 
-public class P_신규아이디추천 {
 
-	public static void main(String[] args) {
-		String answer = "z-+.^.";
-				
+class P_신규아이디추천 {
+    public String solution(String new_id) {
+        
+		String answer = new_id;
+		
+		
 		 // 1. 대문자 -> 소문자로
         answer = answer.toLowerCase();
+//        System.out.println(answer);
         
         //2. 소문자,숫자, -, _, .만 남기고 다 제거
         //^는 not을 의미함. 대괄호 안의 문자들만 남기고 나머지는 다 치환의 대상이 됨.
         answer = answer.replaceAll("[^a-z0-9-_.]","");
+//        System.out.println(answer);
         
         //3. ..은 .으로 치환
         //contains() : 지정한 문자열을 포함하고 있는지 확인함
         while(answer.contains("..")) {
         	answer = answer.replace("..", ".");        	
         }
+//        System.out.println(answer);
         
         //4. .이 처음이나 끝에 있으면 제거
         //charAt() : 문자열의 특정 인덱스에 해당하는 글자를 추출함
@@ -44,27 +47,30 @@ public class P_신규아이디추천 {
         	} else answer = "a";
         }
         
-        
         //5. id가 빈 문자열이면 a 대입
-        if(answer.equals("")) {
+        if(answer.length() == 0) {
         	answer = "a";
         }
+//        System.out.println(answer);
         
         //6. 16자 이상이면 앞에서 15글자까지만 자름
         if(answer.length() >15) {
         	answer = answer.substring(0, 15);
         }
+//        System.out.println(answer);
         
         //7. .이 끝에 위치하면 . 제거
         while(answer.charAt(answer.length()-1) == '.') {
         	answer = answer.substring(0, answer.length()-1);
         }
+//        System.out.println(answer);
         
         //8. id가 2자 이하라면 마지막 문자를 길이가 3이 될 때 까지 반복
         while(answer.length() <= 2) {
         	answer = answer + answer.charAt(answer.length()-1);
         }
-
-	}
-
+	
+        
+        return answer;
+    }
 }
